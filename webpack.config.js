6ef9c13e3a6597c4062138b0b37e1f98716346e6
王亚星 <funnycoderstar@gmail.com>
2017-08-17 11:09:24 +0800

    feat: 添加优化插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     devtool: 'eval-source-map',
     entry: __dirname + '/src/main.js',
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/build',
         filename: 'bundle.js',
     },
     devServer: {
         contentBase: "./dist",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         port: '2000',
-        inline: true//实时刷新
+        inline: true,//实时刷新
         hot: true
     },
     module: {
@@ -25,11 +27,31 @@ module.exports = {
             {
                 test: /\.json$/,
                 loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
         new webpack.BannerPlugin('webpackDemo'),
-        new webpack.HotModuleReplacementPlugin() // 添加热加载插件
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.tmpl.html' // new这个插件的实例,并传入相应的参数
+        }),
+        new webpack.HotModuleReplacementPlugin(), // 添加热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('style.css')
     ]
 }
