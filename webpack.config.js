dc7cd6e9ada3bf7ade0c315b6d7b43ea8bc90199
王亚星 <funnycoderstar@gmail.com>
2017-08-17 11:16:27 +0800

    feat: 添加缓存

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: __dirname + '/src/main.js',
     output: {
         path: __dirname + '/build',
-        filename: 'bundle.js',
+        filename: 'bundle-[hash].js',
     },
     devServer: {
         contentBase: "./dist",//本地服务器所加载的页面所在的目录
